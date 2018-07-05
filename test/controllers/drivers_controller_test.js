b38368a00c9ada775ea5e8266e6205b78938e6e6
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-05 08:48:00 +0200

    test post and put

---
+++ test/controllers/drivers_controller_test.js
@@ -0,0 +1,38 @@
+const assert = require('assert');
+const request = require('supertest');
+const mongoose = require('mongoose');
+const app = require('../../app');
+
+const Driver = mongoose.model('driver');
+
+describe('Drivers controller', () => {
+  it('POST to /api/drivers creates a new driver', done => {
+    Driver.count().then(count => {
+      request(app)
+        .post('/api/drivers')
+        .send({ email: 'test@test.com' })
+        .end(() => {
+          Driver.count().then(newCount => {
+            assert(count + 1 === newCount);
+            done();
+          });
+        });
+    });
+  });
+
+  it('PUT to /api.drivers/id edits an existing driver', done => {
+    const driver = new Driver({ email: 't@t.com', driving: false });
+
+    driver.save().then(() => {
+      request(app)
+        .put(`/api/drivers/${driver._id}`)
+        .send({ driving: true })
+        .end(() => {
+          Driver.findOne({ email: 't@t.com' }).then(driver => {
+            assert(driver.driving === true);
+            done();
+          });
+        });
+    });
+  });
+});
