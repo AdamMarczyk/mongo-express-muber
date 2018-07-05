4ab897ac3bd4603be7c1469494406022c4ab3eeb
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-05 08:35:01 +0200

    add middleware

--- app.js
+++ app.js
@@ -12,4 +12,8 @@ if (process.env.NODE_env !== 'test') {
 app.use(bodyParser.json());
 routes(app);
 
+app.use((err, req, res, next) => {
+  res.status(422).send({ error: err.message });
+});
+
 module.exports = app;
